62eb05c735274d31ec4afd3ec53d4ab6f65ef2b6
BryanLCrowder <47199671+BryanLCrowder@users.noreply.github.com>
2023-01-13 21:51:43 -0500

    puish

--- finace_app/lib/main.dart
+++ finace_app/lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter_complete_guide/widgets/transaction_list.dart';
 import '/widgets/new_transaction.dart';
 
 import 'models/transaction.dart';
+import 'widgets/chart.dart';
 //import 'models/transaction_list.dart';
 
 void main() => runApp(MyApp());
@@ -13,99 +14,120 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Personal Expenses',
       theme: ThemeData(
-        fontFamily: 'Quicksand',
-        textTheme: ThemeData.light().textTheme.copyWith(
-          titleMedium: TextStyle(
-            fontFamily: 'OpenSans', 
-            fontSize: 20, 
-            fontWeight: FontWeight.bold,)
-        ),
-        appBarTheme: AppBarTheme(titleTextStyle: TextStyle(fontFamily: 'OpenSans', fontSize: 30),)
-      ).copyWith(
-        colorScheme: ThemeData().colorScheme.copyWith(
-          primary: Colors.blueGrey,
-          secondary: Colors.amber),
-          ),
-      //theme: ThemeData(
-      //  primarySwatch: Colors.blueGrey,
-      //  accentColor: Colors.amber
-      //),
+          fontFamily: 'Quicksand',
+          textTheme: ThemeData.light().textTheme.copyWith(
+                  titleMedium: TextStyle(
+                fontFamily: 'OpenSans',
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              )),
+          appBarTheme: AppBarTheme(
+            titleTextStyle: TextStyle(fontFamily: 'OpenSans', fontSize: 30),
+          )).copyWith(
+        colorScheme: ThemeData()
+            .colorScheme
+            .copyWith(primary: Colors.blueGrey, secondary: Colors.amber),
+      ),
       home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-final List<Transaction> _userTransactions = [
-  // Transaction(
-  //     id: "Walmart", title: "Groceries", amount: 109.55, date: DateTime.now()),
-  // Transaction(
-  //     id: "Target", title: "Clothes", amount: 19.99, date: DateTime.now()),
-  // Transaction(
-  //     id: "Dick's", title: "Showes", amount: 309.55, date: DateTime.now()),
-];
+  final List<Transaction> _userTransactions = [
+    // Transaction(
+    //     id: "Walmart", title: "Groceries", amount: 109.55, date: DateTime.now()),
+    // Transaction(
+    //     id: "Target", title: "Clothes", amount: 19.99, date: DateTime.now()),
+    // Transaction(
+    //     id: "Dick's", title: "Showes", amount: 309.55, date: DateTime.now()),
+  ];
 
-  void _addNewTransaction(String txtitle, double txamount) {
+  List<Transaction> get _recentTransactions {
+    return _userTransactions.where((tx) {
+      return tx.date.isAfter(
+        DateTime.now().subtract(
+          Duration(days: 7),
+        ),
+      );
+    }).toList();
+  }
+
+  void _addNewTransaction(
+      String txtitle, double txamount, DateTime chosenDate) {
     final newTX = Transaction(
         id: DateTime.now().toString(),
         title: txtitle,
         amount: txamount,
-        date: DateTime.now());
+        date: chosenDate);
 
     setState(() {
       _userTransactions.add(newTX);
     });
   }
 
+  void _deleteTransaction(String id) {
+    setState(() {
+      _userTransactions.removeWhere((tx) {
+        return tx.id == id;
+      });
+    });
+  }
 
-void _startAddNewTransaction(BuildContext ctx) {
-showModalBottomSheet(context: ctx, builder: (_) {
-return GestureDetector(
-  child: NewTransaction(_addNewTransaction),
-  onTap: () {},
-  behavior: HitTestBehavior.opaque,);
-  },);
-}
+  void _startAddNewTransaction(BuildContext ctx) {
+    showModalBottomSheet(
+      context: ctx,
+      builder: (_) {
+        return GestureDetector(
+          child: NewTransaction(_addNewTransaction),
+          onTap: () {},
+          behavior: HitTestBehavior.opaque,
+        );
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
+    final appBar = AppBar(
+      title: Text('Personal Expenses'),
+      actions: <Widget>[
+        IconButton(
+            onPressed: () => _startAddNewTransaction(context),
+            icon: Icon(Icons.add))
+      ],
+    );
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Personal Expenses'),
-        actions: <Widget>[
-          IconButton(onPressed: () =>_startAddNewTransaction(context),icon: Icon(Icons.add))
-        ],
-      ),
+      appBar: appBar,
       body: SingleChildScrollView(
-      child: Container(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Container(
-              width: double.infinity,
-              child: Card(
-                color: Colors.blue,
-                child: Text('Chart'),
-                elevation: 5,
-              ),
-            ),
-            TransactionList(_userTransactions)
-          ],
+        child: Container(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Container(
+                height: (MediaQuery.of(context).size.height - 
+                appBar.preferredSize.height - 
+                MediaQuery.of(context).padding.top) * 0.3,
+                child: Chart(_recentTransactions)),
+              Container(
+                  height: (MediaQuery.of(context).size.height - 
+                  appBar.preferredSize.height - MediaQuery.of(context).padding.top) * 0.7,
+                  child: TransactionList(_userTransactions, _deleteTransaction))
+            ],
+          ),
         ),
       ),
-      ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-      floatingActionButton: 
-      FloatingActionButton(
+      floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
-        onPressed: () =>_startAddNewTransaction(context),),
+        onPressed: () => _startAddNewTransaction(context),
+      ),
     );
   }
 }
--- finace_app/lib/widgets/new_transaction.dart
+++ finace_app/lib/widgets/new_transaction.dart
@@ -1,4 +1,14 @@
+import 'dart:math';
+import 'package:intl/intl.dart';
 import 'package:flutter/material.dart';
+
+int randomNumber() {
+  var random = new Random();
+  int min = 0;
+  int max = 9;
+
+  return min + random.nextInt(max - min);
+}
 
 class NewTransaction extends StatefulWidget {
   final Function addTX;
@@ -11,20 +21,43 @@ class NewTransaction extends StatefulWidget {
 
 class _NewTransactionState extends State<NewTransaction> {
   final titleController = TextEditingController();
-
   final amountController = TextEditingController();
+  DateTime _selectDate;
 
-  void submitData() {
+  void _submitData() {
+    if (amountController.text.isEmpty) {
+      return;
+    }
     final enteredTitle = titleController.text;
     final enteredAmount = double.parse(amountController.text);
 
-    if (enteredTitle.isEmpty || enteredAmount <= 0) {
+    if (enteredTitle.isEmpty || enteredAmount <= 0 || _selectDate == null) {
       return;
     }
 
-    widget.addTX(enteredTitle, enteredAmount);
+    widget.addTX(
+      enteredTitle, 
+      enteredAmount,
+      _selectDate);
 
     Navigator.of(context).pop();
+  }
+
+  void _precentDatePicker() {
+    showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(2023),
+      lastDate: DateTime.now(),
+    ).then((pickedData) {
+      if (pickedData == null) {
+        return;
+      } else {
+        setState(() {
+          _selectDate = pickedData;
+        });
+      }
+    });
   }
 
   @override
@@ -38,15 +71,40 @@ class _NewTransactionState extends State<NewTransaction> {
           children: <Widget>[
             TextField(
                 controller: titleController,
-                onSubmitted: (_) => submitData(),
+                onSubmitted: (_) => _submitData(),
                 decoration: InputDecoration(labelText: "Title")),
             TextField(
                 keyboardType: TextInputType.numberWithOptions(decimal: true),
                 controller: amountController,
-                onSubmitted: (_) => submitData(),
+                onSubmitted: (_) => _submitData(),
                 decoration: InputDecoration(labelText: "Amount")),
+            Container(
+              height: 70,
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                    child: Text(_selectDate == null
+                        ? 'No Date Chosen'
+                        : 'Picked Date: ${DateFormat.yMd().format(_selectDate)}'),
+                  ),
+                  Container(
+                    padding: EdgeInsets.all(10),
+                    child: ElevatedButton(
+                      child: Text(
+                        "Choose Date",
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      onPressed: _precentDatePicker,
+                      style: ElevatedButton.styleFrom(
+                          primary: Theme.of(context).colorScheme.primary,
+                          onPrimary: Colors.white),
+                    ),
+                  )
+                ],
+              ),
+            ),
             TextButton(
-                onPressed: submitData,
+                onPressed: _submitData,
                 style: TextButton.styleFrom(primary: Colors.purple),
                 child: Text("Add Transaction"))
           ],
